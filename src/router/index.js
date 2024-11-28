01ec4419f7494074655a240543a90827d51ff90b
mip <michel@spengergasse.at>
2024-11-28 15:47:57 +0100

    added add bike!
    works!

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,38 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import NotFoundView from '../views/NotFoundView.vue'
-import BikesView from '../views/BikesView.vue'
-import BikeDetailsView from '../views/BikeDetailsView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import NotFoundView from "../views/NotFoundView.vue";
+import BikesView from "../views/BikesView.vue";
+import BikeDetailsView from "../views/BikeDetailsView.vue";
+import AddBikeView from "../views/AddBikeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/bikes',
-      name: 'bikes',
-      component: BikesView
+      path: "/bikes",
+      name: "bikes",
+      component: BikesView,
     },
     {
-      path: '/bikes/:id',
-      name: 'bikeDetails',
-      component: BikeDetailsView
+      path: "/bikes/:id",
+      name: "bikeDetails",
+      component: BikeDetailsView,
+    },
+    {
+      path: "/bikes/add",
+      name: "addBike",
+      component: AddBikeView,
     },
     {
       path: "/:catchAll(.*)",
       component: NotFoundView,
     },
-  ]
-})
+  ],
+});
 
-export default router
+export default router;
